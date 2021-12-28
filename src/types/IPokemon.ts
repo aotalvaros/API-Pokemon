632b6d417ebc506d4ae538f5e7daa1f073222e6e
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-28 16:08:44 -0500

    Se crea una interface llamada IPokemon

---
+++ src/types/IPokemon.ts
@@ -0,0 +1,32 @@
+
+export default interface IPokemon {
+    id: number,
+    name: string,
+    base_experience: number,
+    height: number,
+    types: ITypes,
+    abilities: IAbilities,
+    sprites: ISprites
+};
+
+interface ITypes {
+    type: IType
+};
+
+type IType = {
+    name: string
+};
+
+
+interface IAbilities {
+    ability: IAbility
+};
+
+interface IAbility {
+    name: string
+};
+
+interface ISprites {
+    front_default: string
+};
+
