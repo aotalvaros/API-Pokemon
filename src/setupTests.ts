3522e9dab23bd58b13ffe901dbda69cbc2909bdf
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-28 16:03:54 -0500

    Se realiza la configuracion del setupTest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,14 @@
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+
+import {createSerializer} from 'enzyme-to-json';
+
 //@ts-ignore
 expect.addSnapshotSerializer(createSerializer({
     mode: 'deep',
     noKey: true
 }));
 
-require('jest-fetch-mock').enableMocks();
+Enzyme.configure({ adapter: new Adapter() });
