8bf2ad27fbac946acd1ff3b1d75949a8af46479e
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-29 22:37:41 -0500

    Se modifica la funcion de obtenerPokemonAPI para que la prueba unitaria puedan pasar y se instala yarn

--- src/domain/obtenerPokemonAPI.ts
+++ src/domain/obtenerPokemonAPI.ts
@@ -4,11 +4,11 @@ import IPokemon  from "../types/IPokemon";
 export const obtenerPokemonAPI = (valor: number | string): Promise<IPokemon> => {
    
     return axios.get(`https://pokeapi.co/api/v2/pokemon/${valor}`)
-        .then((response: AxiosResponse<any,IPokemon> ) => response.data ? response.data: "")
-        .catch(resolverError);  ;
+        .then((response: AxiosResponse<IPokemon> ) => response.data)
+        .catch(resolverError);
 };
 
-const resolverError = (error: AxiosError): Promise<string> => {
+const resolverError = (error: AxiosError): Promise<IPokemon> => {
     console.log('Error consumiendo el servicio de obtener Pokemon por => ', error);  
-    return Promise.reject('Error de conexion');   
+    return Promise.reject('Pokemon no existe');   
 };
