e98abc57ebb4f01f071a57039e8f5b2a28e96e40
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-30 16:02:23 -0500

    Se tipa los parametros del todoReducer

--- src/Reducers/todoReducer.ts
+++ src/Reducers/todoReducer.ts
@@ -1,11 +1,12 @@
+import { IAction } from "../types/IPokemonReducer";
 
-export const todoReducer = ( state: any = [], action: any ) => {
+export const todoReducer = ( state: any = [] , action: IAction  ) => {
   
     switch (action.type) {
         case 'obtener':
             return [...state, action.payload]
         default:
             return state;
-    }
+    };
     
 };
