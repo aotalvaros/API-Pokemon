e042beac1fe7c428384dc8321e581d26c446ea5c
andres_otalvaro <aotalvaros@antioquia.gov.co>
2022-01-05 15:22:35 -0500

    Se utiliza el componente del "StyledEngineProvider" en el componente mas alto para poder inyectar estilos personalizados a todos las etiquetas de MUI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 
+import { StyledEngineProvider } from "@mui/material";
 import React from 'react';
 import { Provider } from 'react-redux';
 import { FormularioPokemon } from './components/FormularioPokemon';
 import { store } from './store/store';
+import './styles/styles.scss';
 
 export default function App()  {    
   return (
-    <>
-    <Provider store={store}> 
-      <FormularioPokemon/>  
-    </Provider> 
-    </>
+    <StyledEngineProvider injectFirst>
+      <Provider store={store}> 
+        <FormularioPokemon/>  
+      </Provider> 
+    </StyledEngineProvider>
   );
 };
 
